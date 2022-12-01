3a2fb7f923c790592b83ab483a83b9ee8b79b9b9
misaki arita <misaki.arita0838@gmail.com>
2022-12-01 16:39:13 +0900

    投稿編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,24 @@ class PostsController < ApplicationController
         redirect_to new_post_path
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        redirect_to posts_path
+      end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to post_path, notice: "Your meow edited!"
+        else
+      render :edit
+        end
+    end
+
     private
     
     def post_params
